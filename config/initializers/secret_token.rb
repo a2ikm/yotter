4eb1cb42f7f0337a0adbd2fcb7047276d98c72e4
Masato Ikeda <masato.ikeda@gmail.com>
2012-01-06 00:46:56 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yotter::Application.config.secret_token = '1a3b2a6736f9947f9fbd4f9fe1baee5f6eea5f9793816862ad8da5a9a86a742f1a11433670b16f837d6caa8304cac95154e9014f5a333e959be3eb06d5ce380d'
