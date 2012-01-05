bd59c1164d024eaa03f8c835ebae23132764d61f
Masato Ikeda <masato.ikeda@gmail.com>
2012-01-06 02:15:28 +0900

    fixed typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_or_create_by_omniauth(hash)
+    user = User.find_or_create_by_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url, notice: "Signed in!"
   end
